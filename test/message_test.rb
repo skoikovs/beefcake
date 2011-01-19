8bd7e8e3d0016e5d55a0cf8b7f39a807274c7547
Blake Mizerany <blake.mizerany@gmail.com>
2011-01-19 14:48:17 -0800

    encode/decode message

---
+++ test/message_test.rb
@@ -0,0 +1,28 @@
+require 'beefcake'
+
+class MessageTest < Test::Unit::TestCase
+  def test_numeric
+    #fail "do this!"
+  end
+
+  def test_lendel
+  end
+
+  def test_lendel_composite
+  end
+
+  def test_wire_doesnt_match_decoded_info
+  end
+
+  def test_repeated_field
+  end
+
+  def test_packed_repeated_field
+  end
+
+  def test_required_fields_not_set_on_encode
+  end
+
+  def test_required_fields_not_set_on_decode
+  end
+end
